72fd783adbfc3634cd24f117e8b092da339b28d2
sbs-seenuvasan-mohan <seenuvasan.mohan-contractor@snapon.com>
2024-02-22 18:25:06 +0530

    first commit
    first commit

---
+++ calculator/calculator/App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using NewRelic.Xamarin.Plugin;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace calculator
+{
+    public partial class App : Application
+    {
+       
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+
+            Application.Current.PageAppearing += OnPageAppearing;
+            Application.Current.PageDisappearing += PageDisappearing;
+
+            CrossNewRelicClient.Current.HandleUncaughtException();
+            CrossNewRelicClient.Current.TrackShellNavigatedEvents()
+      // Set optional agent configuration
+      // Options are: crashReportingEnabled, loggingEnabled, logLevel, collectorAddress, crashCollectorAddress
+      AgentStartConfiguration agentConfig = new AgentStartConfiguration(true, true, LogLevel.INFO, "mobile-collector.newrelic.com", "mobile-crash.newrelic.com");
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                //CrossNewRelicClient.Current.Start("<APP-TOKEN-HERE>");
+                // Start with optional agent configuration
+                CrossNewRelicClient.Current.Start("<APP-TOKEN-HERE", agentConfig);
+            }
+            else if (Device.RuntimePlatform == Device.iOS)
+            {
+                //CrossNewRelicClient.Current.Start("<APP-TOKEN-HERE>");
+                // Start with optional agent configuration 
+                CrossNewRelicClient.Current.Start("<APP-TOKEN-HERE", agentConfig);
+            }
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ calculator/calculator/MainPage.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections.Generic;
+using System;
+using Xamarin.Forms;
+
+
+namespace calculator
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        
+        }
+
+        private decimal firstNumber;
+        private String operatorName;
+        private bool isOperatorClicked;
+
+        private void BtnCommon_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            if (LblResult.Text == "0" || isOperatorClicked) {
+
+                isOperatorClicked = false;
+                LblResult.Text = button.Text;
+            }
+            else
+            {
+                LblResult.Text = button.Text;
+            }
+        }
+
+        private void BtnClear_Clicked(object sender, EventArgs e) {
+
+            LblResult.Text = "0";
+            isOperatorClicked = false;
+            firstNumber = 0;
+        
+        }
+
+        private void BtnX_Clicked(object sender, EventArgs e)
+        {
+
+            String number = LblResult.Text;
+            if (number != "0") {
+            
+                number = number.Remove(number.Length - 1,1);
+
+                if (String.IsNullOrEmpty(number))
+                {
+                    LblResult.Text = "0";
+
+                }
+                else
+                {
+                    LblResult.Text =  number;
+                }
+
+
+            }
+        }
+
+        public async void BtnCommonOperation_Clicked(object sender,EventArgs e)
+        {
+            var button = sender as Button;
+            isOperatorClicked = true;
+            operatorName = button.Text;
+            firstNumber = Convert.ToDecimal(LblResult.Text);
+        }
+
+        public async void BtnPercentage_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                String number = LblResult.Text;
+                if (number != "0")
+                {
+                    decimal percentValue = Convert.ToDecimal(number);
+                    String result = (percentValue / 100).ToString("0.##");
+                    LblResult.Text = result;
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+
+        private void BtnEqual_Clicked(object sender, EventArgs e) {
+
+            try
+            {
+                decimal secondNumber = Convert.ToDecimal(LblResult.Text);
+                String finalResult = Calculate(firstNumber,secondNumber).ToString("0.##");
+                LblResult.Text = finalResult;   
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("Error",ex.Message, "OK");     
+            }
+
+        }
+
+        public decimal Calculate(decimal firstNumber, decimal secondNumber)
+        {
+            decimal result = 0;
+            if (operatorName == "+")
+            {
+                result = firstNumber + secondNumber;
+            }
+            else if(operatorName == "-") 
+            {
+                result = firstNumber - secondNumber;
+            }
+            else if (operatorName == "*")
+            {
+                result = firstNumber * secondNumber;
+            }
+            else if (operatorName == "/")
+            {
+                result = firstNumber / secondNumber;
+            }
+            return result;
+        }
+
+
+
+    }
+}
